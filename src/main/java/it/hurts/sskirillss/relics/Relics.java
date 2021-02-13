af99394013ff104d1045c0313a83694f839a579e
SSKirillSS <SSTroveSS@Gmail.com>
2021-02-13 22:00:47 +0300

    Fixed server startup issue

--- src/main/java/it/hurts/sskirillss/relics/Relics.java
+++ src/main/java/it/hurts/sskirillss/relics/Relics.java
@@ -1,27 +1,18 @@
 package it.hurts.sskirillss.relics;
 
-import it.hurts.sskirillss.relics.entities.renderer.*;
 import it.hurts.sskirillss.relics.init.BlockRegistry;
+import it.hurts.sskirillss.relics.init.ClientRegistry;
 import it.hurts.sskirillss.relics.init.EntityRegistry;
 import it.hurts.sskirillss.relics.init.ItemRegistry;
-import it.hurts.sskirillss.relics.items.relics.SpaceDissectorItem;
 import it.hurts.sskirillss.relics.network.NetworkHandler;
-import it.hurts.sskirillss.relics.utils.NBTUtils;
 import it.hurts.sskirillss.relics.utils.Reference;
 import it.hurts.sskirillss.relics.utils.RelicsConfig;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.container.PlayerContainer;
-import net.minecraft.item.ItemModelsProperties;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -32,7 +23,6 @@ import top.theillusivec4.curios.api.SlotTypePreset;
 @Mod(Reference.MODID)
 public class Relics {
     public Relics() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueue);
         MinecraftForge.EVENT_BUS.register(this);
@@ -64,18 +54,6 @@ public class Relics {
         InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE,
                 () -> new SlotTypeMessage.Builder("feet").priority(220)
                         .icon(PlayerContainer.EMPTY_ARMOR_SLOT_BOOTS).build());
-    }
-
-    private void setupClient(final FMLClientSetupEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.STELLAR_CATALYST_PROJECTILE.get(), NullRenderer::new);
-        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.SPACE_DISSECTOR.get(), new SpaceDissectorRenderer.RenderFactory());
-        RenderTypeLookup.setRenderLayer(BlockRegistry.CHALK_BLOCK.get(), RenderType.getCutout());
-
-        ItemModelsProperties.registerProperty(
-                ItemRegistry.SPACE_DISSECTOR.get(), new ResourceLocation(Reference.MODID, "mode"),
-                (stack, world, entity) -> NBTUtils.getBoolean(stack, SpaceDissectorItem.TAG_IS_THROWN, false) ? 2
-                        : (entity instanceof PlayerEntity && ((PlayerEntity) entity).getCooldownTracker().hasCooldown(stack.getItem())) ? 1 : 0
-        );
     }
 
     private void setupCommon(final FMLCommonSetupEvent event) {
---
+++ src/main/java/it/hurts/sskirillss/relics/init/ClientRegistry.java
@@ -0,0 +1,32 @@
+package it.hurts.sskirillss.relics.init;
+
+import it.hurts.sskirillss.relics.entities.renderer.NullRenderer;
+import it.hurts.sskirillss.relics.entities.renderer.SpaceDissectorRenderer;
+import it.hurts.sskirillss.relics.items.relics.SpaceDissectorItem;
+import it.hurts.sskirillss.relics.utils.NBTUtils;
+import it.hurts.sskirillss.relics.utils.Reference;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemModelsProperties;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Reference.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientRegistry {
+    @SubscribeEvent
+    public static void setupClient(final FMLClientSetupEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.STELLAR_CATALYST_PROJECTILE.get(), NullRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.SPACE_DISSECTOR.get(), new SpaceDissectorRenderer.RenderFactory());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.CHALK_BLOCK.get(), RenderType.getCutout());
+
+        ItemModelsProperties.registerProperty(
+                ItemRegistry.SPACE_DISSECTOR.get(), new ResourceLocation(Reference.MODID, "mode"),
+                (stack, world, entity) -> NBTUtils.getBoolean(stack, SpaceDissectorItem.TAG_IS_THROWN, false) ? 2
+                        : (entity instanceof PlayerEntity && ((PlayerEntity) entity).getCooldownTracker().hasCooldown(stack.getItem())) ? 1 : 0);
+    }
+}
