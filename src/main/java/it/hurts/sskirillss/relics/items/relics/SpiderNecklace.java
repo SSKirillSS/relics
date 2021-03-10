6c0ea54234d1827defbb442153ece5c565d054a8
SSKirillSS <SSTroveSS@Gmail.com>
2021-03-10 14:10:32 +0300

    Added Spider Necklace rendering

--- src/main/java/it/hurts/sskirillss/relics/items/relics/SpiderNecklace.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/SpiderNecklace.java
@@ -1,20 +1,28 @@
 package it.hurts.sskirillss.relics.items.relics;
 
 import com.google.common.collect.Lists;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import it.hurts.sskirillss.relics.init.ItemRegistry;
 import it.hurts.sskirillss.relics.items.IHasTooltip;
 import it.hurts.sskirillss.relics.utils.RelicsConfig;
 import it.hurts.sskirillss.relics.utils.RelicsTab;
 import it.hurts.sskirillss.relics.utils.TooltipUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
@@ -49,5 +57,23 @@ public class SpiderNecklace extends Item implements ICurioItem, IHasTooltip {
                 player.fallDistance = 0F;
             }
         }
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing,
+                       float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+        ICurio.RenderHelper.translateIfSneaking(matrixStack, livingEntity);
+        ICurio.RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
+        matrixStack.scale(0.35F, 0.35F, 0.35F);
+        matrixStack.translate(0.0F, 0.25F, -0.4F);
+        matrixStack.rotate(Direction.DOWN.getRotation());
+        Minecraft.getInstance().getItemRenderer()
+                .renderItem(new ItemStack(ItemRegistry.SPIDER_NECKLACE.get()), ItemCameraTransforms.TransformType.NONE, light, OverlayTexture.NO_OVERLAY,
+                        matrixStack, renderTypeBuffer);
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
     }
 }
