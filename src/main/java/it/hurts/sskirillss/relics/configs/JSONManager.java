76fb2a156cacb642431b88d12aec597f6ae9ddb2
SSKirillSS <SSTroveSS@Gmail.com>
2021-06-04 17:23:58 +0300

    Implemented parsing of JSON configs

--- src/main/java/it/hurts/sskirillss/relics/configs/JSONManager.java
+++ src/main/java/it/hurts/sskirillss/relics/configs/JSONManager.java
@@ -1,7 +1,6 @@
 package it.hurts.sskirillss.relics.configs;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import it.hurts.sskirillss.relics.configs.variables.level.RelicLevel;
 import it.hurts.sskirillss.relics.configs.variables.worldgen.RelicLoot;
 import it.hurts.sskirillss.relics.init.ItemRegistry;
@@ -13,6 +12,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,17 +25,31 @@ public class JSONManager {
         try {
             Path dir = FMLPaths.CONFIGDIR.get().resolve("relics").resolve("stats");
             Files.createDirectories(dir);
-            for (RegistryObject<Item> object : ItemRegistry.ITEMS.getEntries()) {
-                if (!object.isPresent()) continue;
-                Item item = object.get();
+            for (RegistryObject<Item> registryObject : ItemRegistry.ITEMS.getEntries()) {
+                if (!registryObject.isPresent()) continue;
+                Item item = registryObject.get();
                 if (!(item instanceof RelicItem)) continue;
                 RelicItem relic = (RelicItem) item;
-                RelicStat stat = new RelicStat(new RelicLoot(relic.getLootChests().stream().map(ResourceLocation::toString).collect(Collectors.toList()),
-                        relic.getWorldgenChance()), new RelicLevel(relic.getMaxLevel(), relic.getInitialExp(), relic.getExpRatio()));
-                RelicUtils.Level.LEVEL.put(relic, stat.getLevel());
-                RelicUtils.Worldgen.LOOT.put(relic, stat.getLoot());
-                try (Writer writer = Files.newBufferedWriter(dir.resolve(item.getRegistryName().getPath() + ".json"))) {
+                Path path = dir.resolve(item.getRegistryName().getPath() + "." + "json");
+                RelicLevel defaultLevel = new RelicLevel(relic.getMaxLevel(), relic.getInitialExp(), relic.getExpRatio());
+                RelicLoot defaultLoot = new RelicLoot(relic.getLootChests().stream().map(ResourceLocation::toString)
+                        .collect(Collectors.toList()), relic.getWorldgenChance());
+                RelicStat stat = new RelicStat(defaultLoot, defaultLevel);
+                if (!Files.exists(path)) try (Writer writer = Files.newBufferedWriter(path)) {
                     SERIALIZER.toJson(stat, writer);
+                    continue;
+                }
+                try (Reader reader = Files.newBufferedReader(path)) {
+                    stat = SERIALIZER.fromJson(reader, RelicStat.class);
+                    RelicLevel level = stat.getLevel();
+                    RelicLoot loot = stat.getLoot();
+                    if (level == null) level = defaultLevel;
+                    if (loot == null) loot = defaultLoot;
+                    try (Writer writer = Files.newBufferedWriter(path)) {
+                        SERIALIZER.toJson(new RelicStat(loot, level), writer);
+                    }
+                    RelicUtils.Level.LEVEL.put(relic, level);
+                    RelicUtils.Worldgen.LOOT.put(relic, loot);
                 }
             }
         } catch (
