43c5d7a9a1b709d79a69f45a66c17b7538cac26e
SSKirillSS <SSTroveSS@Gmail.com>
2021-06-18 22:25:02 +0300

    Added Runic Altar JEI compat

---
+++ src/main/java/it/hurts/sskirillss/relics/compat/jei/RelicsJEIPlugin.java
@@ -0,0 +1,37 @@
+package it.hurts.sskirillss.relics.compat.jei;
+
+import it.hurts.sskirillss.relics.compat.jei.categories.RunicAltarCategory;
+import it.hurts.sskirillss.relics.crafting.RunicAltarRecipe;
+import it.hurts.sskirillss.relics.utils.Reference;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class RelicsJEIPlugin implements IModPlugin {
+    public static final ResourceLocation UID = new ResourceLocation(Reference.MODID, "jei_plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return UID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
+
+        registration.addRecipeCategories(new RunicAltarCategory(guiHelper));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ClientWorld world = Minecraft.getInstance().level;
+        if (world == null) return;
+        registration.addRecipes(world.getRecipeManager().getAllRecipesFor(RunicAltarRecipe.RECIPE), RunicAltarCategory.UID);
+    }
+}
---
+++ src/main/java/it/hurts/sskirillss/relics/compat/jei/categories/RunicAltarCategory.java
@@ -0,0 +1,76 @@
+package it.hurts.sskirillss.relics.compat.jei.categories;
+
+import it.hurts.sskirillss.relics.crafting.RunicAltarRecipe;
+import it.hurts.sskirillss.relics.init.BlockRegistry;
+import it.hurts.sskirillss.relics.utils.Reference;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
+
+public class RunicAltarCategory implements IRecipeCategory<RunicAltarRecipe> {
+    public static final ResourceLocation UID = new ResourceLocation(Reference.MODID, "runic_altar");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(Reference.MODID, "textures/gui/jei/runic_altar.png");
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public RunicAltarCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 144, 85);
+        this.icon = helper.createDrawableIngredient(new ItemStack(BlockRegistry.RUNIC_ALTAR_BLOCK.get()));
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
+
+    @Override
+    public Class<? extends RunicAltarRecipe> getRecipeClass() {
+        return RunicAltarRecipe.class;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Runic Infusion";
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public void setIngredients(RunicAltarRecipe runicAltarRecipe, IIngredients iIngredients) {
+        iIngredients.setOutput(VanillaTypes.ITEM, runicAltarRecipe.getResultItem());
+        iIngredients.setInputIngredients(runicAltarRecipe.getIngredients());
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayout iRecipeLayout, RunicAltarRecipe runicAltarRecipe, IIngredients iIngredients) {
+        IGuiItemStackGroup stacks = iRecipeLayout.getItemStacks();
+        List<List<ItemStack>> inputs = iIngredients.getInputs(VanillaTypes.ITEM);
+        List<List<ItemStack>> outputs = iIngredients.getOutputs(VanillaTypes.ITEM);
+
+        stacks.init(0, true, 32, 32);
+        stacks.init(1, true, 0, 32);
+        stacks.init(2, true, 32, 0);
+        stacks.init(3, true, 32, 64);
+        stacks.init(4, true, 64, 32);
+        stacks.init(5, false, 120, 32);
+
+        for (int i = 0; i < inputs.size(); i++) stacks.set(i, inputs.get(i));
+        stacks.set(5, outputs.get(0));
+    }
+}
