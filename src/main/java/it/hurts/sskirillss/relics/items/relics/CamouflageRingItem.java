759c6beac5a4671a818aad1a72f325f0d3b04342
SSKirillSS <SSTroveSS@Gmail.com>
2021-06-20 22:42:45 +0300

    Camouflage Ring rewrite

--- src/main/java/it/hurts/sskirillss/relics/items/relics/CamouflageRingItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/CamouflageRingItem.java
@@ -12,23 +12,25 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CamouflageRingItem extends RelicItem<CamouflageRingItem.Stats> implements ICurioItem {
     public static final String TAG_TIME = "time";
-    public static final String TAG_IS_ACTIVE = "active";
 
     public static CamouflageRingItem INSTANCE;
 
@@ -47,24 +49,17 @@ public class CamouflageRingItem extends RelicItem<CamouflageRingItem.Stats> impl
 
     @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        if (livingEntity.tickCount % 20 != 0 || !(livingEntity instanceof PlayerEntity)) return;
+        PlayerEntity player = (PlayerEntity) livingEntity;
         int time = NBTUtils.getInt(stack, TAG_TIME, 0);
-        if (NBTUtils.getBoolean(stack, TAG_IS_ACTIVE, false)) {
-            if (time > 0 && livingEntity.isShiftKeyDown()
-                    && livingEntity.getCommandSenderWorld().getBlockState(livingEntity.blockPosition()).getBlock() instanceof DoublePlantBlock) {
-                if (livingEntity.tickCount % 20 == 0) NBTUtils.setInt(stack, TAG_TIME, time - 1);
-                livingEntity.setInvisible(true);
-            } else {
-                NBTUtils.setBoolean(stack, TAG_IS_ACTIVE, false);
-                livingEntity.setInvisible(false);
-            }
+        if (canHide(player)) {
+            player.addEffect(new EffectInstance(Effects.INVISIBILITY, 20, 0, false, false));
+            NBTUtils.setInt(stack, TAG_TIME, time + 1);
         } else {
-            if (livingEntity.tickCount % 20 == 0 && time < config.invisibilityTime) {
-                NBTUtils.setInt(stack, TAG_TIME, time + 1);
-            }
-            if (livingEntity.isShiftKeyDown() && time > 0
-                    && livingEntity.getCommandSenderWorld().getBlockState(livingEntity.blockPosition()).getBlock() instanceof DoublePlantBlock) {
-                NBTUtils.setBoolean(stack, TAG_IS_ACTIVE, true);
-            }
+            if (time <= 0) return;
+            if (!player.getCooldowns().isOnCooldown(stack.getItem()))
+                player.getCooldowns().addCooldown(stack.getItem(), config.invisibilityTime * 20);
+            NBTUtils.setInt(stack, TAG_TIME, time - 1);
         }
     }
 
@@ -78,29 +73,27 @@ public class CamouflageRingItem extends RelicItem<CamouflageRingItem.Stats> impl
         return Stats.class;
     }
 
+    private static boolean canHide(LivingEntity entity) {
+        Stats config = INSTANCE.config;
+        if (!(entity instanceof PlayerEntity)) return false;
+        PlayerEntity player = (PlayerEntity) entity;
+        Optional<ImmutableTriple<String, Integer, ItemStack>> optional = CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), player);
+        if (optional.isPresent()) {
+            ItemStack stack = optional.get().getRight();
+            return player.getCommandSenderWorld().getBlockState(player.blockPosition()).getBlock() instanceof DoublePlantBlock && player.isCrouching()
+                    && NBTUtils.getInt(stack, TAG_TIME, 0) < config.invisibilityTime && !player.getCooldowns().isOnCooldown(stack.getItem());
+        }
+        return false;
+    }
+
     @Mod.EventBusSubscriber(modid = Reference.MODID)
     public static class CamouflageRingServerEvents {
         @SubscribeEvent
         public static void onEntityHurt(LivingHurtEvent event) {
             Stats config = INSTANCE.config;
-            if (event.getSource().getEntity() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) event.getSource().getEntity();
-                if (CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), player).isPresent()) {
-                    ItemStack stack = CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), player).get().getRight();
-                    if (NBTUtils.getBoolean(stack, TAG_IS_ACTIVE, false)) {
-                        event.setAmount(event.getAmount() * config.damageMultiplier);
-                    }
-                }
-            }
-        }
-
-        @SubscribeEvent
-        public static void onBlockBreak(BlockEvent.BreakEvent event) {
-            if (event.getState().getBlock() instanceof DoublePlantBlock
-                    && CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), event.getPlayer()).isPresent()
-                    && NBTUtils.getBoolean(CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), event.getPlayer()).get().getRight(), TAG_IS_ACTIVE, false)) {
-                event.setCanceled(true);
-            }
+            if (!(event.getSource().getEntity() instanceof PlayerEntity)) return;
+            PlayerEntity player = (PlayerEntity) event.getSource().getEntity();
+            if (canHide(player)) event.setAmount(event.getAmount() * config.damageMultiplier);
         }
     }
 
@@ -109,9 +102,7 @@ public class CamouflageRingItem extends RelicItem<CamouflageRingItem.Stats> impl
         @SubscribeEvent
         public static void onEntityRender(RenderPlayerEvent.Pre event) {
             PlayerEntity player = event.getPlayer();
-            if (CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), player).isPresent()
-                    && NBTUtils.getBoolean(CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.CAMOUFLAGE_RING.get(), player).get().getRight(), TAG_IS_ACTIVE, false))
-                event.setCanceled(true);
+            if (canHide(player)) event.setCanceled(true);
         }
     }
 
