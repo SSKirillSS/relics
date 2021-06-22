839e46bf1d7a36dfd4b4bd28f8dc1b2fd1a7f2fa
SSKirillSS <SSTroveSS@Gmail.com>
2021-06-22 13:18:57 +0300

    Holy Locket rework

--- src/main/java/it/hurts/sskirillss/relics/items/relics/HolyLocketItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/HolyLocketItem.java
@@ -7,11 +7,13 @@ import it.hurts.sskirillss.relics.items.RelicItem;
 import it.hurts.sskirillss.relics.utils.RelicUtils;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,7 +25,7 @@ public class HolyLocketItem extends RelicItem<HolyLocketItem.Stats> {
     public static HolyLocketItem INSTANCE;
 
     public HolyLocketItem() {
-        super(Rarity.UNCOMMON);
+        super(Rarity.RARE);
 
         INSTANCE = this;
     }
@@ -32,6 +34,7 @@ public class HolyLocketItem extends RelicItem<HolyLocketItem.Stats> {
     public List<ITextComponent> getShiftTooltip(ItemStack stack) {
         List<ITextComponent> tooltip = Lists.newArrayList();
         tooltip.add(new TranslationTextComponent("tooltip.relics.holy_locket.shift_1"));
+        tooltip.add(new TranslationTextComponent("tooltip.relics.holy_locket.shift_2"));
         return tooltip;
     }
 
@@ -46,20 +49,40 @@ public class HolyLocketItem extends RelicItem<HolyLocketItem.Stats> {
     }
 
     @Mod.EventBusSubscriber
-    static class HolyLocketServerEvents {
+    static class HolyLocketEvents {
         @SubscribeEvent
         public static void onLivingHurt(LivingHurtEvent event) {
             Stats config = INSTANCE.config;
+            Item item = ItemRegistry.HOLY_LOCKET.get();
             if (event.getSource().getEntity() instanceof PlayerEntity) {
                 PlayerEntity player = (PlayerEntity) event.getSource().getEntity();
                 LivingEntity entity = event.getEntityLiving();
-                if (CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.HOLY_LOCKET.get(), player).isPresent()
-                        && entity.isInvertedHealAndHarm()) {
-                    if (player.getCommandSenderWorld().random.nextFloat() <= config.igniteChance)
-                        entity.setSecondsOnFire(config.burnDuration);
+                if (!entity.isInvertedHealAndHarm()) return;
+                CuriosApi.getCuriosHelper().findEquippedCurio(item, player).ifPresent(triple -> {
+                    if (player.getCommandSenderWorld().random.nextFloat() <= config.igniteChance) entity.setSecondsOnFire(config.burnDuration);
                     event.setAmount(event.getAmount() * config.damageMultiplier);
-                }
+                });
+            } else if (event.getEntityLiving() instanceof PlayerEntity) {
+                PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+                CuriosApi.getCuriosHelper().findEquippedCurio(item, player).ifPresent(triple -> {
+                    if (random.nextFloat() <= config.healChance) {
+                        player.heal(event.getAmount());
+                        event.setCanceled(true);
+                        return;
+                    }
+                    if (player.getCooldowns().isOnCooldown(item)) event.setCanceled(true);
+                    else player.getCooldowns().addCooldown(item, config.invulnerabilityTime);
+                });
             }
+        }
+
+        @SubscribeEvent
+        public static void onLivingDamage(LivingAttackEvent event) {
+            Item item = ItemRegistry.HOLY_LOCKET.get();
+            if (!(event.getEntityLiving() instanceof PlayerEntity)) return;
+            PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+            if (CuriosApi.getCuriosHelper().findEquippedCurio(item, player).isPresent()
+                    && player.getCooldowns().isOnCooldown(item)) event.setCanceled(true);
         }
     }
 
@@ -67,5 +90,7 @@ public class HolyLocketItem extends RelicItem<HolyLocketItem.Stats> {
         public float damageMultiplier = 1.5F;
         public float igniteChance = 0.25F;
         public int burnDuration = 4;
+        public float healChance = 0.1F;
+        public int invulnerabilityTime = 10;
     }
 }
