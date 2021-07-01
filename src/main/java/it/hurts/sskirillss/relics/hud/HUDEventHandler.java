977f7da77b069c3675883b8267856621027036ee
SSKirillSS <SSTroveSS@Gmail.com>
2021-07-01 23:43:25 +0300

    Added cooldown support to relics abilities HUD

--- src/main/java/it/hurts/sskirillss/relics/hud/HUDEventHandler.java
+++ src/main/java/it/hurts/sskirillss/relics/hud/HUDEventHandler.java
@@ -11,14 +11,19 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -81,23 +86,8 @@ public class HUDEventHandler {
         }
         for (int i = offset; i < Math.min(relics.size(), slots) + offset; i++) {
             if (i >= relics.size() || i < 0) break;
-            matrix.pushPose();
-            RenderSystem.enableBlend();
-            RenderSystem.color4f(1.0F, 1.0F, 1.0F, Math.min(1.0F, animation * 0.025F));
-            String path = relics.get(i).getLeft().getItem().getRegistryName().getPath();
-            if (path.equals(Items.AIR.getRegistryName().getPath())) continue;
-            ResourceLocation RELIC = new ResourceLocation(Reference.MODID, "textures/items/" + path + ".png");
-            manager.bind(SLOT);
-            AbstractGui.blit(matrix, x - 3, y - 3, 22, 28, 0F, 0F, 1, 1, 1, 1);
-            manager.bind(RELIC);
-            AbstractGui.blit(matrix, x, y, 16, 16, 0F, 0F, 1, 1, 1, 1);
-            KeyBinding key = keyBindings.get(i - offset);
-            matrix.scale(0.5F, 0.5F, 0.5F);
-            Minecraft.getInstance().font.draw(matrix, key.getKeyModifier().getCombinedName(key.getKey(),
-                    () -> key.getKey().getDisplayName()), x * 2.0F - 3, (y + 19) * 2.0F, 0xFFFFFF);
-            RenderSystem.disableBlend();
-            matrix.popPose();
-            y += 29;
+            drawRelic(matrix, manager, relics.get(i).getKey().getItem(), keyBindings.get(i - offset), player, x, y);
+            y += 33;
         }
         if (relics.size() > slots) {
             RenderSystem.enableBlend();
@@ -107,6 +97,44 @@ public class HUDEventHandler {
             RenderSystem.disableBlend();
         }
         manager.bind(AbstractGui.GUI_ICONS_LOCATION);
+    }
+
+    private static void drawRelic(MatrixStack matrix, TextureManager manager, Item item, KeyBinding key, PlayerEntity player, int x, int y) {
+        String path = item.getRegistryName().getPath();
+        if (path.equals(Items.AIR.getRegistryName().getPath())) return;
+        ResourceLocation RELIC = new ResourceLocation(Reference.MODID, "textures/items/" + path + ".png");
+
+        matrix.pushPose();
+        RenderSystem.enableBlend();
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, Math.min(1.0F, animation * 0.025F));
+
+        manager.bind(SLOT);
+        AbstractGui.blit(matrix, x - 4, y - 4, 24, 32, 0F, 0F, 1, 1, 1, 1);
+
+        manager.bind(RELIC);
+        AbstractGui.blit(matrix, x, y, 16, 16, 0F, 0F, 1, 1, 1, 1);
+        RenderSystem.disableDepthTest();
+        RenderSystem.disableTexture();
+        RenderSystem.defaultBlendFunc();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder builder = tessellator.getBuilder();
+        float yOff = y + MathHelper.floor(16.0F * (1.0F - player.getCooldowns().getCooldownPercent(item, Minecraft.getInstance().getFrameTime())));
+        float offset = MathHelper.ceil(16.0F * player.getCooldowns().getCooldownPercent(item, Minecraft.getInstance().getFrameTime()));
+        builder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        builder.vertex(x, yOff + 0, 0.0D).color(255, 255, 255, 127).endVertex();
+        builder.vertex(x, yOff + offset, 0.0D).color(255, 255, 255, 127).endVertex();
+        builder.vertex(x + 16, yOff + offset, 0.0D).color(255, 255, 255, 127).endVertex();
+        builder.vertex(x + 16, yOff + 0, 0.0D).color(255, 255, 255, 127).endVertex();
+        Tessellator.getInstance().end();
+        RenderSystem.enableTexture();
+        RenderSystem.enableDepthTest();
+
+        matrix.scale(0.5F, 0.5F, 0.5F);
+        Minecraft.getInstance().font.draw(matrix, key.getKeyModifier().getCombinedName(key.getKey(),
+                () -> key.getKey().getDisplayName()), x * 2.0F - 2, (y + 21) * 2.0F, 0xFFFFFF);
+
+        RenderSystem.disableBlend();
+        matrix.popPose();
     }
 
     @SubscribeEvent
@@ -120,7 +148,7 @@ public class HUDEventHandler {
                 ItemStack stack = handler.getStackInSlot(i);
                 if (!(stack.getItem() instanceof RelicItem)) continue;
                 RelicItem relic = (RelicItem) stack.getItem();
-                if (!relic.hasAbility()) return;
+                if (!relic.hasAbility()) continue;
                 relics.add(new ImmutablePair<>(stack, i));
             }
             offset = offset / slots * slots;
