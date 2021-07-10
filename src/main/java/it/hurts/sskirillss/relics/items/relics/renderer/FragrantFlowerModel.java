711d371bf0d2262a9f42ada4a09495f2dbd6c5b8
SSKirillSS <SSTroveSS@Gmail.com>
2021-07-10 14:18:57 +0300

    Changed Fragrant Flower model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/FragrantFlowerModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/FragrantFlowerModel.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
 public class FragrantFlowerModel extends BipedModel<LivingEntity> {
-
     public FragrantFlowerModel() {
         super(1.0F, 0, 16, 16);
 
@@ -17,41 +16,41 @@ public class FragrantFlowerModel extends BipedModel<LivingEntity> {
         center.setPos(-1.0F, 25.0F, 0.0F);
         head.addChild(center);
 
-        ModelRenderer petal_1 = new ModelRenderer(this);
-        petal_1.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_1);
-        setRotationAngle(petal_1, 0.3491F, 0.0F, -0.6545F);
-        petal_1.texOffs(0, 0).addBox(7.5F, -7.5F, -2.7F, 2.0F, 2.0F, 0.0F, 0.0F, false);
+        ModelRenderer cube_r1 = new ModelRenderer(this);
+        cube_r1.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r1);
+        setRotationAngle(cube_r1, 0.3491F, 0.0F, -0.6545F);
+        cube_r1.texOffs(4, 4).addBox(7.5F, -7.5F, -2.7F, 2.0F, 2.0F, 0.0F, 0.0F, false);
 
-        ModelRenderer petal_2 = new ModelRenderer(this);
-        petal_2.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_2);
-        setRotationAngle(petal_2, 0.2618F, -0.5672F, 0.0873F);
-        petal_2.texOffs(0, 2).addBox(-2.2F, -11.25F, -2.7F, 2.0F, 2.0F, 0.0F, 0.0F, false);
+        ModelRenderer cube_r2 = new ModelRenderer(this);
+        cube_r2.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r2);
+        setRotationAngle(cube_r2, 0.2618F, -0.5672F, 0.0873F);
+        cube_r2.texOffs(4, 2).addBox(-2.2F, -11.25F, -2.7F, 2.0F, 2.0F, 0.0F, 0.0F, false);
 
-        ModelRenderer petal_3 = new ModelRenderer(this);
-        petal_3.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_3);
-        setRotationAngle(petal_3, 0.0F, 0.0F, -0.7854F);
-        petal_3.texOffs(3, 3).addBox(7.6F, -3.5F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        ModelRenderer cube_r3 = new ModelRenderer(this);
+        cube_r3.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r3);
+        setRotationAngle(cube_r3, 0.0F, 0.0F, -0.7854F);
+        cube_r3.texOffs(4, 0).addBox(7.6F, -3.5F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
-        ModelRenderer petal_4 = new ModelRenderer(this);
-        petal_4.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_4);
-        setRotationAngle(petal_4, 0.2182F, 0.5672F, 0.7418F);
-        petal_4.texOffs(4, 0).addBox(-0.25F, -11.25F, -3.0F, 2.0F, 2.0F, 0.0F, 0.0F, false);
+        ModelRenderer cube_r4 = new ModelRenderer(this);
+        cube_r4.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r4);
+        setRotationAngle(cube_r4, 0.2182F, 0.5672F, 0.7418F);
+        cube_r4.texOffs(0, 4).addBox(-0.25F, -11.25F, -3.0F, 2.0F, 2.0F, 0.0F, 0.0F, false);
 
-        ModelRenderer petal_5 = new ModelRenderer(this);
-        petal_5.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_5);
-        setRotationAngle(petal_5, 0.2182F, -0.5672F, -0.7418F);
-        petal_5.texOffs(0, 5).addBox(2.0F, -6.5F, -7.0F, 2.0F, 2.0F, 0.0F, 0.0F, false);
+        ModelRenderer cube_r5 = new ModelRenderer(this);
+        cube_r5.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r5);
+        setRotationAngle(cube_r5, 0.2182F, -0.5672F, -0.7418F);
+        cube_r5.texOffs(0, 2).addBox(2.0F, -6.5F, -7.0F, 2.0F, 2.0F, 0.0F, 0.0F, false);
 
-        ModelRenderer petal_6 = new ModelRenderer(this);
-        petal_6.setPos(0.0F, -24.0F, 0.0F);
-        center.addChild(petal_6);
-        setRotationAngle(petal_6, -0.3927F, 0.0F, -0.1745F);
-        petal_6.texOffs(4, 5).addBox(3.9F, -4.75F, -6.8F, 2.0F, 2.0F, 0.0F, 0.0F, false);
+        ModelRenderer cube_r6 = new ModelRenderer(this);
+        cube_r6.setPos(0.0F, -24.0F, 0.0F);
+        center.addChild(cube_r6);
+        setRotationAngle(cube_r6, -0.3927F, 0.0F, -0.1745F);
+        cube_r6.texOffs(0, 0).addBox(3.9F, -4.75F, -6.8F, 2.0F, 2.0F, 0.0F, 0.0F, false);
     }
 
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
