9a0351d8d16d606511a3da5a68e3f14b689d63e6
SSKirillSS <SSTroveSS@Gmail.com>
2021-07-12 00:35:10 +0300

    Changed Old Boot model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/OldBootModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/OldBootModel.java
@@ -11,13 +11,15 @@ public class OldBootModel extends BipedModel<LivingEntity> {
         setAllVisible(false);
 
         leftLeg = new ModelRenderer(this);
-        leftLeg.setPos(2.0F, 12.0F, 0.0F);
-        leftLeg.texOffs(0, 0).addBox(-2.5F, 10.1F, -4.5F, 5.0F, 2.0F, 7.0F, 0.0F, false);
-        leftLeg.texOffs(0, 9).addBox(-2.5F, 7.1F, -2.5F, 5.0F, 3.0F, 5.0F, 0.0F, false);
-
         rightLeg = new ModelRenderer(this);
-        rightLeg.setPos(2.0F, 12.0F, 0.0F);
-        rightLeg.texOffs(0, 0).addBox(-2.5F, 10.1F, -4.5F, 5.0F, 2.0F, 7.0F, 0.0F, false);
-        rightLeg.texOffs(0, 9).addBox(-2.5F, 7.1F, -2.5F, 5.0F, 3.0F, 5.0F, 0.0F, false);
+        ModelRenderer model = new ModelRenderer(this);
+        model.setPos(-2.0F, 12.0F, 0.0F);
+        leftLeg.addChild(model);
+        rightLeg.addChild(model);
+        model.texOffs(0, 0).addBox(-0.5F, -7.999F, -2.5F, 5.0F, 8.0F, 5.0F, 0.0F, false);
+        model.texOffs(15, 0).addBox(-0.5F, -3.0F, -4.5F, 5.0F, 3.0F, 2.0F, 0.0F, false);
+        model.texOffs(0, 13).addBox(-1.0F, -5.25F, -3.0F, 6.0F, 1.0F, 6.0F, 0.0F, false);
+        model.texOffs(18, 13).addBox(0.5F, -5.75F, -3.25F, 3.0F, 2.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 21).addBox(-1.0F, -8.5F, -3.0F, 6.0F, 2.0F, 6.0F, 0.0F, false);
     }
 }
