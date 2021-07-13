f9bbb715809220b562a492ee6b0c71489fd0051b
SSKirillSS <SSTroveSS@Gmail.com>
2021-07-14 00:26:13 +0300

    Added Amphibian Boot model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/boots/AmphibianBootItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/boots/AmphibianBootItem.java
@@ -1,11 +1,16 @@
 package it.hurts.sskirillss.relics.items.relics.boots;
 
 import com.google.common.collect.Lists;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import it.hurts.sskirillss.relics.configs.variables.stats.RelicStats;
 import it.hurts.sskirillss.relics.items.RelicItem;
+import it.hurts.sskirillss.relics.items.relics.renderer.AmphibianBootModel;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.Reference;
 import it.hurts.sskirillss.relics.utils.RelicUtils;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
@@ -18,6 +23,7 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.ForgeMod;
 import org.apache.commons.lang3.tuple.MutablePair;
 import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
@@ -66,6 +72,25 @@ public class AmphibianBootItem extends RelicItem<AmphibianBootItem.Stats> implem
     @Override
     public Class<Stats> getConfigClass() {
         return Stats.class;
+    }
+
+    private final ResourceLocation TEXTURE = new ResourceLocation(Reference.MODID, "textures/items/models/amphibian_boot.png");
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+        AmphibianBootModel model = new AmphibianBootModel();
+        matrixStack.pushPose();
+        model.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        model.prepareMobModel(livingEntity, limbSwing, limbSwingAmount, partialTicks);
+        ICurio.RenderHelper.followBodyRotations(livingEntity, model);
+        model.renderToBuffer(matrixStack, renderTypeBuffer.getBuffer(RenderType.entityTranslucent(TEXTURE)),
+                light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        matrixStack.popPose();
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
     }
 
     public static class Stats extends RelicStats {
