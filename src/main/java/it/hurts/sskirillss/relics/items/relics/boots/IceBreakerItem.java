b29a0beda9dc15e630c7c0c33f74c3e8c943cba6
SSKirillSS <SSTroveSS@Gmail.com>
2021-07-13 13:00:21 +0300

    Added Ice Breaker model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/boots/IceBreakerItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/boots/IceBreakerItem.java
@@ -1,12 +1,17 @@
 package it.hurts.sskirillss.relics.items.relics.boots;
 
 import com.google.common.collect.Lists;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import it.hurts.sskirillss.relics.configs.variables.stats.RelicStats;
 import it.hurts.sskirillss.relics.init.ItemRegistry;
 import it.hurts.sskirillss.relics.items.RelicItem;
+import it.hurts.sskirillss.relics.items.relics.renderer.IceBreakerModel;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.Reference;
 import it.hurts.sskirillss.relics.utils.RelicUtils;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
@@ -28,6 +33,7 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.commons.lang3.tuple.MutablePair;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
@@ -89,6 +95,25 @@ public class IceBreakerItem extends RelicItem<IceBreakerItem.Stats> implements I
     @Override
     public Class<Stats> getConfigClass() {
         return Stats.class;
+    }
+
+    private final ResourceLocation TEXTURE = new ResourceLocation(Reference.MODID, "textures/items/models/ice_breaker.png");
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+        IceBreakerModel model = new IceBreakerModel();
+        matrixStack.pushPose();
+        model.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        model.prepareMobModel(livingEntity, limbSwing, limbSwingAmount, partialTicks);
+        ICurio.RenderHelper.followBodyRotations(livingEntity, model);
+        model.renderToBuffer(matrixStack, renderTypeBuffer.getBuffer(RenderType.entityTranslucent(TEXTURE)),
+                light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        matrixStack.popPose();
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
     }
 
     @Mod.EventBusSubscriber(modid = Reference.MODID)
---
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/IceBreakerModel.java
@@ -0,0 +1,38 @@
+package it.hurts.sskirillss.relics.items.relics.renderer;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+
+public class IceBreakerModel extends BipedModel<LivingEntity> {
+    public IceBreakerModel() {
+        super(1.0F, 0, 32, 32);
+
+        setAllVisible(false);
+
+        leftLeg = new ModelRenderer(this);
+        rightLeg = new ModelRenderer(this);
+        ModelRenderer model = new ModelRenderer(this);
+        model.setPos(-2.0F, 12.0F, 0.0F);
+        leftLeg.addChild(model);
+        rightLeg.addChild(model);
+        model.texOffs(0, 0).addBox(-0.5F, -7.999F, -2.5F, 5.0F, 8.0F, 5.0F, 0.0F, false);
+        model.texOffs(15, 0).addBox(-0.5F, -3.0F, -4.5F, 5.0F, 3.0F, 2.0F, 0.0F, false);
+        model.texOffs(0, 13).addBox(-1.0F, -8.5F, -3.0F, 6.0F, 2.0F, 6.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(0.0F, 0.0F, -4.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(3.0F, 0.0F, -4.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(1.5F, 0.0F, -4.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(1.5F, 0.0F, -3.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(0.0F, 0.0F, -3.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(3.0F, 0.0F, -3.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(0.0F, 0.0F, 1.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(1.5F, 0.0F, 1.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(3.0F, 0.0F, 1.25F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(1.5F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(3.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(0.0F, 0.0F, -1.8F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(1.5F, 0.0F, -1.8F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(3.0F, 0.0F, -1.8F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+    }
+}
