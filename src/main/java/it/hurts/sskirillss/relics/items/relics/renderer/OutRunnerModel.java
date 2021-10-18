d1e8594cfda723a43586d19d2f3fa80e03cf06db
SSKirillSS <SSTroveSS@gmail.com>
2021-10-18 18:22:14 +0300

    Resprited Out Runner model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/OutRunnerModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/OutRunnerModel.java
@@ -1,43 +1,36 @@
 package it.hurts.sskirillss.relics.items.relics.renderer;
 
+import it.hurts.sskirillss.relics.utils.RenderUtils;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
 public class OutRunnerModel extends BipedModel<LivingEntity> {
     public OutRunnerModel() {
-        super(1.0F, 0, 32, 32);
+        super(1.0F, 0, 64, 64);
 
         setAllVisible(false);
 
         leftLeg = new ModelRenderer(this);
         rightLeg = new ModelRenderer(this);
         ModelRenderer model = new ModelRenderer(this);
-        model.setPos(-2.0F, 11.6F, 0.0F);
+        model.setPos(-2.0F, 12.0F, 0.0F);
         leftLeg.addChild(model);
         rightLeg.addChild(model);
-        model.texOffs(0, 0).addBox(-0.5F, -7.999F, -2.5F, 5.0F, 8.0F, 5.0F, 0.0F, false);
-        model.texOffs(15, 0).addBox(-0.5F, -3.0F, -4.5F, 5.0F, 3.0F, 2.0F, 0.0F, false);
-        model.texOffs(0, 13).addBox(-1.0F, -8.5F, -3.0F, 6.0F, 2.0F, 6.0F, 0.0F, false);
+        model.texOffs(0, 9).addBox(-1.0F, -5.0F, -3.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
+        model.texOffs(18, 9).addBox(-1.0F, -2.0F, -5.0F, 6.0F, 3.0F, 2.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(-1.5F, -7.0F, -3.5F, 7.0F, 2.0F, 7.0F, 0.0F, false);
 
         ModelRenderer cube_r1 = new ModelRenderer(this);
-        cube_r1.setPos(-2.0F, -12.0F, 1.4142F);
+        cube_r1.setPos(0.0F, 0.0F, 0.0F);
         model.addChild(cube_r1);
-        setRotationAngle(cube_r1, 0.0F, 0.7854F, 0.0F);
-        cube_r1.texOffs(0, 0).addBox(-1.7F, 6.0F, 1.8358F, 2.0F, 4.0F, 0.0F, 0.0F, false);
-        cube_r1.texOffs(9, 21).addBox(-2.7F, 5.5F, 1.8358F, 3.0F, 5.0F, 0.0F, 0.0F, false);
+        RenderUtils.setRotationAngle(cube_r1, 0.0F, 0.4363F, 0.0F);
+        cube_r1.texOffs(4, 21).addBox(2.275F, -5.0F, 4.825F, 1.0F, 4.0F, 3.0F, 0.0F, true);
 
         ModelRenderer cube_r2 = new ModelRenderer(this);
-        cube_r2.setPos(-2.0F, -12.0F, 1.4142F);
+        cube_r2.setPos(0.0F, 0.0F, 0.0F);
         model.addChild(cube_r2);
-        setRotationAngle(cube_r2, 0.0F, -0.7854F, 0.0F);
-        cube_r2.texOffs(0, 13).addBox(5.3F, 6.0F, -3.83F, 2.0F, 4.0F, 0.0F, 0.0F, false);
-        cube_r2.texOffs(3, 21).addBox(5.3F, 5.5F, -3.82F, 3.0F, 5.0F, 0.0F, 0.0F, false);
-    }
-
-    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.xRot = x;
-        modelRenderer.yRot = y;
-        modelRenderer.zRot = z;
+        RenderUtils.setRotationAngle(cube_r2, 0.0F, -0.4363F, 0.0F);
+        cube_r2.texOffs(21, 0).addBox(0.35F, -5.0F, 3.075F, 1.0F, 4.0F, 3.0F, 0.0F, false);
     }
 }
