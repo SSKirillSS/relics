33f740ad3652512214fbd8f370c15e931e2f945c
SSKirillSS <SSTroveSS@gmail.com>
2021-10-21 02:20:29 +0300

    Resprited Ice Skates model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/IceSkatesModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/IceSkatesModel.java
@@ -6,27 +6,27 @@ import net.minecraft.entity.LivingEntity;
 
 public class IceSkatesModel extends BipedModel<LivingEntity> {
     public IceSkatesModel() {
-        super(1.0F, 0, 32, 32);
+        super(1.0F, 0, 64, 64);
 
         setAllVisible(false);
 
         leftLeg = new ModelRenderer(this);
         rightLeg = new ModelRenderer(this);
         ModelRenderer model = new ModelRenderer(this);
-        model.setPos(-2.0F, 11.6F, 0.0F);
+        model.setPos(-2.0F, 12.0F, 0.0F);
         leftLeg.addChild(model);
         rightLeg.addChild(model);
-        model.texOffs(0, 8).addBox(-0.5F, -5.999F, -2.5F, 5.0F, 6.0F, 5.0F, 0.0F, false);
-        model.texOffs(18, 0).addBox(-0.5F, -3.0F, -4.5F, 5.0F, 3.0F, 2.0F, 0.0F, false);
-        model.texOffs(13, 12).addBox(1.5F, 0.0F, -4.0F, 1.0F, 1.0F, 7.0F, 0.0F, false);
-        model.texOffs(0, 3).addBox(1.5F, -1.425F, -5.415F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-        model.texOffs(0, 0).addBox(-1.0F, -7.0F, -3.0F, 6.0F, 2.0F, 6.0F, 0.0F, false);
+        model.texOffs(0, 9).addBox(-1.0F, -5.0F, -3.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
+        model.texOffs(0, 21).addBox(-1.0F, -2.0F, -5.0F, 6.0F, 3.0F, 2.0F, 0.0F, false);
+        model.texOffs(0, 0).addBox(-1.5F, -7.0F, -3.5F, 7.0F, 2.0F, 7.0F, 0.0F, false);
+        model.texOffs(16, 13).addBox(1.5F, 0.745F, -5.005F, 1.0F, 1.0F, 8.0F, 0.0F, false);
+        model.texOffs(28, 5).addBox(1.5F, -1.065F, -5.58F, 1.0F, 2.0F, 1.0F, 0.0F, false);
 
         ModelRenderer cube_r1 = new ModelRenderer(this);
-        cube_r1.setPos(-2.0F, -11.0F, 0.0F);
+        cube_r1.setPos(0.0F, 1.75F, 0.0F);
         model.addChild(cube_r1);
-        setRotationAngle(cube_r1, -0.7854F, 0.0F, 0.0F);
-        cube_r1.texOffs(0, 0).addBox(3.5F, 10.315F, 3.655F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+        setRotationAngle(cube_r1, 0.6109F, 0.0F, 0.0F);
+        cube_r1.texOffs(28, 5).addBox(1.5F, -3.875F, -4.1F, 1.0F, 1.0F, 1.0F, 0.0F, false);
     }
 
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
