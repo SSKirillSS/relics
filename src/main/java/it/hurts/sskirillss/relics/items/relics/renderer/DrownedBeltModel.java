7c4c771a3631f67f23194c444090691357382223
SSKirillSS <SSTroveSS@gmail.com>
2021-10-20 19:46:26 +0300

    Resprited Drowned Belt model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/DrownedBeltModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/DrownedBeltModel.java
@@ -5,21 +5,17 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
 public class DrownedBeltModel extends BipedModel<LivingEntity> {
-    public DrownedBeltModel() {
-        super(1.0F, 0, 32, 32);
+	public DrownedBeltModel() {
+		super(1.0F, 0, 32, 32);
 
-        setAllVisible(false);
+		setAllVisible(false);
 
-        body = new ModelRenderer(this);
+		body = new ModelRenderer(this);
+		ModelRenderer model = new ModelRenderer(this);
+		model.setPos(0.0F, 25.0F, 0.0F);
+		body.addChild(model);
 
-        ModelRenderer belt = new ModelRenderer(this);
-        belt.setPos(0.0F, 24.0F, 0.0F);
-        body.addChild(belt);
-
-        belt.texOffs(0, 0).addBox(-4.5F, -14.0F, -2.5F, 9.0F, 2.0F, 5.0F, 0.0F, false);
-        belt.texOffs(0, 9).addBox(-2.0F, -14.5F, -3.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-        belt.texOffs(0, 0).addBox(1.0F, -14.5F, -3.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-        belt.texOffs(6, 7).addBox(-1.0F, -12.5F, -3.0F, 2.0F, 1.0F, 1.0F, 0.0F, false);
-        belt.texOffs(0, 7).addBox(-1.0F, -14.5F, -3.0F, 2.0F, 1.0F, 1.0F, 0.0F, false);
-    }
+		model.texOffs(0, 0).addBox(-4.5F, -15.5F, -2.5F, 9.0F, 3.0F, 5.0F, 0.0F, false);
+		model.texOffs(0, 8).addBox(-1.5F, -15.5F, -3.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+	}
 }
