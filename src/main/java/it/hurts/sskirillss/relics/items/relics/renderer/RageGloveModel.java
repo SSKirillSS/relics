07bb73d1f37065884db517dad745ba5e11c8d6a3
SSKirillSS <SSTroveSS@gmail.com>
2021-10-23 18:51:30 +0300

    Resprited Rage Glove model

--- src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/RageGloveModel.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/renderer/RageGloveModel.java
@@ -12,18 +12,20 @@ public class RageGloveModel extends BipedModel<LivingEntity> {
 
         rightArm = new ModelRenderer(this);
         ModelRenderer right = new ModelRenderer(this);
-        right.setPos(5.0F, 22.0F, 0.0F);
+        right.setPos(4.5F, 22.0F, 0.0F);
         rightArm.addChild(right);
-        right.texOffs(0, 10).addBox(-8.5F, -16.999F, -2.5F, 5.0F, 5.0F, 5.0F, 0.0F, false);
-        right.texOffs(0, 0).addBox(-8.5F, -16.75F, -2.75F, 5.0F, 5.0F, 5.0F, 0.0F, false);
-        right.texOffs(0, 20).addBox(-9.0F, -17.0F, -3.0F, 6.0F, 1.0F, 6.0F, 0.0F, false);
+        right.texOffs(0, 10).addBox(-8.0F, -16.75F, -2.5F, 5.0F, 5.0F, 5.0F, 0.0F, false);
+        right.texOffs(0, 20).addBox(-8.5F, -16.75F, -3.0F, 6.0F, 1.0F, 6.0F, 0.0F, false);
+        right.texOffs(15, 6).addBox(-6.875F, -16.625F, -2.625F, 4.0F, 5.0F, 4.0F, 0.0F, false);
+        right.texOffs(1, 1).addBox(-8.125F, -16.6249F, -2.6251F, 4.0F, 5.0F, 4.0F, 0.0F, false);
 
         leftArm = new ModelRenderer(this);
         ModelRenderer left = new ModelRenderer(this);
-        left.setPos(7.0F, 22.0F, 0.0F);
+        left.setPos(6.5F, 22.0F, 0.0F);
         leftArm.addChild(left);
-        left.texOffs(0, 10).addBox(-8.5F, -16.999F, -2.5F, 5.0F, 5.0F, 5.0F, 0.0F, false);
-        left.texOffs(0, 0).addBox(-8.5F, -16.75F, -2.75F, 5.0F, 5.0F, 5.0F, 0.0F, false);
-        left.texOffs(0, 20).addBox(-9.0F, -17.0F, -3.0F, 6.0F, 1.0F, 6.0F, 0.0F, false);
+        left.texOffs(0, 10).addBox(-8.0F, -16.75F, -2.5F, 5.0F, 5.0F, 5.0F, 0.0F, false);
+        left.texOffs(0, 20).addBox(-8.5F, -16.75F, -3.0F, 6.0F, 1.0F, 6.0F, 0.0F, false);
+        left.texOffs(15, 6).addBox(-6.875F, -16.625F, -2.625F, 4.0F, 5.0F, 4.0F, 0.0F, false);
+        left.texOffs(1, 1).addBox(-8.125F, -16.6249F, -2.6251F, 4.0F, 5.0F, 4.0F, 0.0F, false);
     }
 }
