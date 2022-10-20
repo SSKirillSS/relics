b05fc63fb89cdb724e84a0110689037b1aadda72
SSKirillSS <SSTroveSS@Gmail.com>
2022-10-20 19:59:23 +0300

    Added Leather Belt description

--- src/main/java/it/hurts/sskirillss/relics/items/relics/belt/LeatherBeltItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/belt/LeatherBeltItem.java
@@ -4,6 +4,11 @@ import it.hurts.sskirillss.relics.client.tooltip.base.RelicStyleData;
 import it.hurts.sskirillss.relics.items.relics.base.data.base.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicSlotModifier;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityData;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityEntry;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityStat;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicLevelingData;
+import it.hurts.sskirillss.relics.utils.MathUtils;
 import net.minecraft.world.item.ItemStack;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -11,8 +16,19 @@ public class LeatherBeltItem extends RelicItem {
     @Override
     public RelicData getRelicData() {
         return RelicData.builder()
+                .abilityData(RelicAbilityData.builder()
+                        .ability("slots", RelicAbilityEntry.builder()
+                                .requiredPoints(1)
+                                .stat("talisman", RelicAbilityStat.builder()
+                                        .initialValue(1D, 3D)
+                                        .upgradeModifier("add", 1D)
+                                        .formatValue(value -> String.valueOf((int) (MathUtils.round(value, 1))))
+                                        .build())
+                                .build())
+                        .build())
+                .levelingData(new RelicLevelingData(100, 10, 100))
                 .styleData(RelicStyleData.builder()
-                        .borders("#a24f1d", "#4b1c00")
+                        .borders("#32a167", "#16702e")
                         .build())
                 .build();
     }
@@ -20,7 +36,7 @@ public class LeatherBeltItem extends RelicItem {
     @Override
     public RelicSlotModifier getSlotModifiers(ItemStack stack) {
         return RelicSlotModifier.builder()
-                .entry(Pair.of("talisman", 3))
+                .entry(Pair.of("talisman", (int) Math.round(getAbilityValue(stack, "slots", "talisman"))))
                 .build();
     }
 }
