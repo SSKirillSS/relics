947d060cc8acf6ea302906e256163db4ad1d431d
SSKirillSS <SSTroveSS@Gmail.com>
2022-11-26 14:43:55 +0300

    Fixed server startup issue

--- src/main/java/it/hurts/sskirillss/relics/tiles/base/IHasHUDInfo.java
+++ src/main/java/it/hurts/sskirillss/relics/tiles/base/IHasHUDInfo.java
@@ -2,10 +2,7 @@ package it.hurts.sskirillss.relics.tiles.base;
 
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
-@OnlyIn(Dist.CLIENT)
 public interface IHasHUDInfo {
     void renderHUDInfo(PoseStack poseStack, Window window);
 }
