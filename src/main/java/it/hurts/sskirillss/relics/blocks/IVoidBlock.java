ec77279c77bc3c1a227ffc719b66c99429fcd2f6
SSKirillSS <SSTroveSS@Gmail.com>
2022-11-28 12:59:04 +0300

    Fixed Researching Table registry issue

--- src/main/java/it/hurts/sskirillss/relics/blocks/IVoidBlock.java
+++
@@ -1,4 +0,0 @@
-package it.hurts.sskirillss.relics.blocks;
-
-public interface IVoidBlock {
-}
--- src/main/java/it/hurts/sskirillss/relics/init/BlockRegistry.java
+++ src/main/java/it/hurts/sskirillss/relics/init/BlockRegistry.java
@@ -2,6 +2,9 @@ package it.hurts.sskirillss.relics.init;
 
 import it.hurts.sskirillss.relics.blocks.ResearchingTableBlock;
 import it.hurts.sskirillss.relics.utils.Reference;
+import it.hurts.sskirillss.relics.utils.RelicsTab;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -12,10 +15,16 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod.EventBusSubscriber(modid = Reference.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BlockRegistry {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MODID);
 
     public static final RegistryObject<ResearchingTableBlock> RESEARCHING_TABLE = BLOCKS.register("researching_table", ResearchingTableBlock::new);
 
     public static void registerBlocks() {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        for (RegistryObject<? extends Block> block : BLOCKS.getEntries())
+            ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(RelicsTab.RELICS_TAB)));
+
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
