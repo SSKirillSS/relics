e4cde4152fabca02bc97278533f4193830337b2a
SSKirillSS <SSTroveSS@Gmail.com>
2023-04-10 11:54:54 +0300

    Removed possibility to enchant relics

--- src/main/java/it/hurts/sskirillss/relics/items/relics/base/RelicItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/base/RelicItem.java
@@ -36,8 +36,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
@@ -108,11 +106,6 @@ public abstract class RelicItem extends Item implements ICurioItem {
         }
 
         return super.onEntityItemUpdate(stack, entity);
-    }
-
-    @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
-        return enchantment == Enchantments.UNBREAKING;
     }
 
     @Override
