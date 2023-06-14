b37016137afc43715ced563e91462ce9abc0c2f2
SSKirillSS <SSTroveSS@Gmail.com>
2023-06-14 08:30:18 +0300

    Fixed player's researches syncing on the dimension change

--- src/main/java/it/hurts/sskirillss/relics/capability/handlers/SyncHandler.java
+++ src/main/java/it/hurts/sskirillss/relics/capability/handlers/SyncHandler.java
@@ -33,4 +33,15 @@ public class SyncHandler {
 
         NetworkHandler.sendToClient(new CapabilitySyncPacket(CapabilityUtils.getRelicsCapability(player).serializeNBT()), (ServerPlayer) player);
     }
+
+    @SubscribeEvent
+    public static void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
+        Player player = event.getEntity();
+        Level level = player.getCommandSenderWorld();
+
+        if (level.isClientSide())
+            return;
+
+        NetworkHandler.sendToClient(new CapabilitySyncPacket(CapabilityUtils.getRelicsCapability(player).serializeNBT()), (ServerPlayer) player);
+    }
 }
