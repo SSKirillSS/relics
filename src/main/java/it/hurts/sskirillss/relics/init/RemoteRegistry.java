f80cb826d23148434b0b3125130130c8521a920a
SSKirillSS <SSTroveSS@Gmail.com>
2023-08-26 14:45:17 +0300

    Added HunterBelt model

--- src/main/java/it/hurts/sskirillss/relics/init/RemoteRegistry.java
+++ src/main/java/it/hurts/sskirillss/relics/init/RemoteRegistry.java
@@ -3,10 +3,12 @@ package it.hurts.sskirillss.relics.init;
 import com.mojang.blaze3d.vertex.PoseStack;
 import it.hurts.sskirillss.relics.client.hud.abilities.AbilitiesRenderHandler;
 import it.hurts.sskirillss.relics.client.models.items.DrownedBeltModel;
+import it.hurts.sskirillss.relics.client.models.items.HunterBeltModel;
 import it.hurts.sskirillss.relics.client.particles.circle.CircleTintFactory;
 import it.hurts.sskirillss.relics.client.particles.spark.SparkTintFactory;
 import it.hurts.sskirillss.relics.client.renderer.entities.*;
-import it.hurts.sskirillss.relics.client.renderer.items.handlers.DrownedBeltRenderer;
+import it.hurts.sskirillss.relics.client.renderer.items.DrownedBeltRenderer;
+import it.hurts.sskirillss.relics.client.renderer.items.HunterBeltRenderer;
 import it.hurts.sskirillss.relics.client.renderer.tiles.ResearchingTableRenderer;
 import it.hurts.sskirillss.relics.items.SolidSnowballItem;
 import it.hurts.sskirillss.relics.items.relics.BlazingFlaskItem;
@@ -112,11 +114,13 @@ public class RemoteRegistry {
         });
 
         CuriosRendererRegistry.register(ItemRegistry.DROWNED_BELT.get(), DrownedBeltRenderer::new);
+        CuriosRendererRegistry.register(ItemRegistry.HUNTER_BELT.get(), HunterBeltRenderer::new);
     }
 
     @SubscribeEvent
     public static void registerLayers(final EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(DrownedBeltModel.LAYER, DrownedBeltModel::createLayer);
+        event.registerLayerDefinition(HunterBeltModel.LAYER, HunterBeltModel::createLayer);
     }
 
     @SubscribeEvent
