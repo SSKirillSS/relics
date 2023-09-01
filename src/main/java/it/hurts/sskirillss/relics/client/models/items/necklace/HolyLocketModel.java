f6c5c389b808c73c551d52da42b5b4118a3c5e6b
SSKirillSS <SSTroveSS@Gmail.com>
2023-09-01 18:23:47 +0300

    Updated Holy Locket model

--- src/main/java/it/hurts/sskirillss/relics/client/models/items/necklace/HolyLocketModel.java
+++ src/main/java/it/hurts/sskirillss/relics/client/models/items/necklace/HolyLocketModel.java
@@ -29,11 +29,11 @@ public class HolyLocketModel extends HumanoidModel<LivingEntity> {
 
         PartDefinition part = mesh.getRoot();
 
-        PartDefinition bone = part.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(0, 3).addBox(-8.0F, -1.15F, -4.15F, 16.0F, 7.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition bone = part.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(0, 3).addBox(-8.0F, -1.15F, -4.15F, 16.0F, 7.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 1.15F, 0.0F));
 
-        bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 18).addBox(-2.5917F, -0.8822F, -0.525F, 5.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0354F, 7.9071F, -4.575F, 0.0F, 0.0F, 0.2568F));
-        bone.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(12, 18).addBox(-2.95F, -3.025F, 2.425F, 2.0F, 2.0F, 1.0F, new CubeDeformation(-0.15F)), PartPose.offsetAndRotation(0.0347F, 10.3F, -8.125F, 0.0F, 0.0F, 0.7854F));
-        bone.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0104F, -2.6071F, -0.55F, 2.0F, 6.0F, 1.0F, new CubeDeformation(0.05F)), PartPose.offsetAndRotation(0.0354F, 7.9071F, -4.575F, 0.0F, 0.0F, -0.004F));
+        bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 18).addBox(-2.6096F, -0.8646F, -0.2F, 5.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0571F, 7.8947F, -4.9F, 0.0F, 0.0F, 0.2568F));
+        bone.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(12, 18).addBox(-1.0F, -1.0F, -0.5F, 2.0F, 2.0F, 1.0F, new CubeDeformation(-0.15F)), PartPose.offsetAndRotation(0.0877F, 7.4893F, -5.2F, 0.0F, 0.0F, 0.7854F));
+        bone.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0322F, -2.5947F, -0.225F, 2.0F, 6.0F, 1.0F, new CubeDeformation(0.05F)), PartPose.offsetAndRotation(0.0571F, 7.8947F, -4.9F, 0.0F, 0.0F, -0.004F));
 
         return LayerDefinition.create(mesh, 64, 64);
     }
