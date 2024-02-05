7f7d06555015f2ff52f3bf7c01f88fc2c42cbdc8
SSKirillSS <sstrovess@gmail.com>
2024-02-05 16:06:50 +0100

    Made Researching Table rotatable

--- src/main/java/it/hurts/sskirillss/relics/blocks/ResearchingTableBlock.java
+++ src/main/java/it/hurts/sskirillss/relics/blocks/ResearchingTableBlock.java
@@ -12,15 +12,18 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -29,9 +32,10 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ResearchingTableBlock extends Block implements EntityBlock {
+public class ResearchingTableBlock extends HorizontalDirectionalBlock implements EntityBlock {
     public ResearchingTableBlock() {
         super(Properties.of(Material.WOOD)
                 .lightLevel((s) -> 15)
@@ -72,6 +76,20 @@ public class ResearchingTableBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new ResearchingTableTile(pos, state);
+    }
+
+    @Override
+    public void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+
+        builder.add(FACING);
+    }
+
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Mod.EventBusSubscriber
