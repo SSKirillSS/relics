1fe122f6d1cbdde414f33a53d9837c18d936880d
SSKirillSS <sstrovess@gmail.com>
2024-02-05 11:39:54 +0100

    Added maximize and minimize commands

--- src/main/java/it/hurts/sskirillss/relics/commands/RelicsCommand.java
+++ src/main/java/it/hurts/sskirillss/relics/commands/RelicsCommand.java
@@ -9,14 +9,20 @@ import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.base.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityEntry;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityStat;
 import it.hurts.sskirillss.relics.items.relics.base.utils.AbilityUtils;
+import it.hurts.sskirillss.relics.items.relics.base.utils.DataUtils;
 import it.hurts.sskirillss.relics.items.relics.base.utils.LevelingUtils;
+import it.hurts.sskirillss.relics.items.relics.base.utils.QualityUtils;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.server.command.EnumArgument;
+
+import java.util.Map;
 
 import java.util.Map;
 
@@ -38,9 +44,71 @@ public class RelicsCommand {
 
                                             LevelingUtils.setLevel(stack, IntegerArgumentType.getInteger(context, "level"));
 
-                                            return Command.SINGLE_SUCCESS;
-                                        })))
-                        .then(Commands.literal("add")
+                                    return Command.SINGLE_SUCCESS;
+                                })))
+                .then(Commands.literal("maximize")
+                        .executes(context -> {
+                            ServerPlayer player = context.getSource().getPlayerOrException();
+                            ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
+
+                            if (!(stack.getItem() instanceof RelicItem)) {
+                                context.getSource().sendFailure(Component.translatable("command.relics.base.not_relic"));
+
+                                return 0;
+                            }
+
+                            RelicData relicData = DataUtils.getRelicData(stack.getItem());
+
+                            LevelingUtils.setLevel(stack, relicData.getLevelingData().getMaxLevel());
+
+                            for (Map.Entry<String, RelicAbilityEntry> abilityEntry : relicData.getAbilityData().getAbilities().entrySet()) {
+                                String abilityId = abilityEntry.getKey();
+                                RelicAbilityEntry abilityInfo = abilityEntry.getValue();
+
+                                AbilityUtils.setAbilityPoints(stack, abilityId, abilityInfo.getMaxLevel());
+
+                                for (Map.Entry<String, RelicAbilityStat> statEntry : abilityInfo.getStats().entrySet()) {
+                                    String statId = statEntry.getKey();
+
+                                    AbilityUtils.setAbilityValue(stack, abilityId, statId, QualityUtils.getStatByQuality(stack.getItem(), abilityId, statId, QualityUtils.MAX_QUALITY));
+                                }
+                            }
+
+                            return Command.SINGLE_SUCCESS;
+                        })
+                )
+                .then(Commands.literal("minimize")
+                        .executes(context -> {
+                            ServerPlayer player = context.getSource().getPlayerOrException();
+                            ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
+
+                            if (!(stack.getItem() instanceof RelicItem)) {
+                                context.getSource().sendFailure(Component.translatable("command.relics.base.not_relic"));
+
+                                return 0;
+                            }
+
+                            RelicData relicData = DataUtils.getRelicData(stack.getItem());
+
+                            LevelingUtils.setLevel(stack, relicData.getLevelingData().getMaxLevel());
+
+                            for (Map.Entry<String, RelicAbilityEntry> abilityEntry : relicData.getAbilityData().getAbilities().entrySet()) {
+                                String abilityId = abilityEntry.getKey();
+
+                                AbilityUtils.setAbilityPoints(stack, abilityId, 0);
+
+                                for (Map.Entry<String, RelicAbilityStat> statEntry : abilityEntry.getValue().getStats().entrySet()) {
+                                    String statId = statEntry.getKey();
+
+                                    AbilityUtils.setAbilityValue(stack, abilityId, statId, QualityUtils.getStatByQuality(stack.getItem(), abilityId, statId, 0));
+                                }
+                            }
+
+                            return Command.SINGLE_SUCCESS;
+                        })
+                )
+                .then(Commands.literal("level")
+                        .then(Commands.argument("action", EnumArgument.enumArgument(CommandAction.class))
                                 .then(Commands.argument("level", IntegerArgumentType.integer())
                                         .executes(context -> {
                                             ServerPlayer player = context.getSource().getPlayerOrException();
