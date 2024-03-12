9bdf9cb0134545bf5906d58c6f96f6d4a11b19d1
SSKirillSS <SSTroveSS@Gmail.com>
2024-03-12 06:44:52 +0300

    Resolved backport issues

--- src/main/java/it/hurts/sskirillss/relics/items/relics/base/RelicItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/base/RelicItem.java
@@ -6,14 +6,12 @@ import it.hurts.sskirillss.relics.items.relics.base.data.RelicAttributeModifier;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicSlotModifier;
 import it.hurts.sskirillss.relics.utils.RelicsTab;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
@@ -61,26 +59,7 @@ public abstract class RelicItem extends Item implements ICurioItem, IRelicItem {
     }
 
     @Override
-    public boolean isDamageable(ItemStack stack) {
-        return stack.getMaxDamage() > 0;
-    }
-
-    // Mojank moment
-
-    @Override
-    public int getBarWidth(ItemStack stack) {
-        return Math.round(13.0F - (float) stack.getDamageValue() * 13.0F / (float) getMaxDamage(stack));
-    }
-
-    @Override
-    public int getBarColor(@NotNull ItemStack stack) {
-//        Triple<String, String, String> color = getStyle(stack).getDurability();
-//
-//        if (color == null)
-        return Mth.hsvToRgb(Math.max(0F, ((float) getMaxDamage(stack) - (float) stack.getDamageValue()) / (float) getMaxDamage(stack)) / 3F, 1F, 1F);
-//
-//        float percentage = stack.getDamageValue() * 100F / getMaxDamage(stack);
-//
-//        return Color.decode(percentage < 33.3F ? color.getLeft() : percentage < 66.6F ? color.getMiddle() : color.getRight()).getRGB();
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return true;
     }
 }
--- src/main/java/it/hurts/sskirillss/relics/mixin/MixinScreen.java
+++ src/main/java/it/hurts/sskirillss/relics/mixin/MixinScreen.java
@@ -1,7 +1,6 @@
 package it.hurts.sskirillss.relics.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import it.hurts.sskirillss.relics.api.events.common.TooltipDisplayEvent;
 import it.hurts.sskirillss.relics.client.screen.base.ITickingWidget;
 import it.hurts.sskirillss.relics.client.screen.description.data.base.ParticleData;
 import it.hurts.sskirillss.relics.client.screen.utils.ParticleStorage;
@@ -9,10 +8,6 @@ import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.events.AbstractContainerEventHandler;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.event.RenderTooltipEvent;
-import net.minecraftforge.common.MinecraftForge;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,7 +15,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,20 +22,9 @@ import java.util.Map;
 
 @Mixin(Screen.class)
 public class MixinScreen extends AbstractContainerEventHandler {
-    @Shadow(remap = false)
-    private final ItemStack tooltipStack = ItemStack.EMPTY;
-
     @Final
     @Shadow
     private List<GuiEventListener> children;
-
-    // FIXME 1.19.2 :: Still needed?
-    @Inject(method = "renderTooltipInternal", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;blitOffset:F", ordinal = 2, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void renderTooltipInternal(PoseStack matrix, List<ClientTooltipComponent> components, int preX, int preY, CallbackInfo info, RenderTooltipEvent.Pre pre, int width, int height, int postX, int postY) {
-        TooltipDisplayEvent event = new TooltipDisplayEvent(tooltipStack, matrix, width, height, postX, postY);
-
-        MinecraftForge.EVENT_BUS.post(event);
-    }
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void tick(CallbackInfo ci) {
