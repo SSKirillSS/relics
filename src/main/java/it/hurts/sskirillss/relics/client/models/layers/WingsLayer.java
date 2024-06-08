7924c4f4c5a8cc4d7866f56dfcbac8fe022428fc
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-06-08 10:02:51 +0300

    1.20.1 re (#138)
    * new diameter particle
    
    * New color and new logic
    
    * New speed

--- src/main/java/it/hurts/sskirillss/relics/client/models/layers/WingsLayer.java
+++ src/main/java/it/hurts/sskirillss/relics/client/models/layers/WingsLayer.java
@@ -3,6 +3,10 @@ package it.hurts.sskirillss.relics.client.models.layers;
 import com.mojang.blaze3d.vertex.PoseStack;
 import it.hurts.sskirillss.relics.client.models.parts.HaloModel;
 import it.hurts.sskirillss.relics.client.models.parts.WingsModel;
+import it.hurts.sskirillss.relics.init.ItemRegistry;
+import it.hurts.sskirillss.relics.items.relics.necklace.HolyLocketItem;
+import it.hurts.sskirillss.relics.utils.EntityUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -11,6 +15,7 @@ import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class WingsLayer<T extends LivingEntity, M extends EntityModel<T>> extends RenderLayer<T, M> {
@@ -26,6 +31,11 @@ public class WingsLayer<T extends LivingEntity, M extends EntityModel<T>> extend
 
     @Override
     public void render(PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, T pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+        ItemStack stackLocal = EntityUtils.findEquippedCurio(Minecraft.getInstance().player, ItemRegistry.HOLY_LOCKET.get());
+
+        if (!(stackLocal.getItem() instanceof HolyLocketItem relic) || !relic.isAbilityOnCooldown(stackLocal, "blessing"))
+            return;
+
         pPoseStack.pushPose();
 
         pPoseStack.scale(0.75F, 0.75F, 0.75F);
