6362ec4e64573df0cf2ec7f04b400c1b5be4af2f
SSKirillSS <SSTroveSS@Gmail.com>
2024-09-17 05:19:18 +0300

    Fixed vanishing effect on server

--- src/main/java/it/hurts/sskirillss/relics/effects/VanishingEffect.java
+++ src/main/java/it/hurts/sskirillss/relics/effects/VanishingEffect.java
@@ -2,11 +2,13 @@ package it.hurts.sskirillss.relics.effects;
 
 import it.hurts.sskirillss.relics.init.EffectRegistry;
 import it.hurts.sskirillss.relics.utils.Reference;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderHandEvent;
 import net.neoforged.neoforge.client.event.RenderLivingEvent;
 
 public class VanishingEffect extends MobEffect {
@@ -19,6 +21,12 @@ public class VanishingEffect extends MobEffect {
         @SubscribeEvent
         public static void onEntityRender(RenderLivingEvent.Pre<?, ?> event) {
             if (event.getEntity().hasEffect(EffectRegistry.VANISHING))
+                event.setCanceled(true);
+        }
+
+        @SubscribeEvent
+        public static void onHandRender(RenderHandEvent event) {
+            if (Minecraft.getInstance().player.hasEffect(EffectRegistry.VANISHING) && event.getItemStack().isEmpty())
                 event.setCanceled(true);
         }
     }
